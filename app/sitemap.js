0f8cb335c841620f4b87cbc1b462a2db10a6d22f
Ishaan18singh <singhishaan2004@icloud.com>
2026-07-29 00:16:24 +0530

    Include individual project detail pages in sitemap.xml

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,10 +1,20 @@
+import { SITE_DATA } from '@/lib/data';
+import { slugify } from '@/lib/utils';
+
 const SITE_URL = 'https://www.ashwanikumarias.com';
 
 const ROUTES = ['/', '/about', '/timeline', '/projects', '/awards', '/publications', '/ias-aspirants', '/gallery', '/contact'];
 
 export default function sitemap() {
-  return ROUTES.map((route) => ({
+  const staticEntries = ROUTES.map((route) => ({
     url: `${SITE_URL}${route}`,
     lastModified: new Date()
   }));
+
+  const projectEntries = SITE_DATA.projects.map((project) => ({
+    url: `${SITE_URL}/projects/${slugify(project.title)}`,
+    lastModified: new Date()
+  }));
+
+  return [...staticEntries, ...projectEntries];
 }
